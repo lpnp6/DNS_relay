9cf0a8f4321bb61e2610e7b4545874321a0182de
lpnp6 <117889360+lpnp6@users.noreply.github.com>
2023-06-24 00:30:42 +0800

    Update dns_server.h

--- dns_server.h
+++ dns_server.h
@@ -1,1 +1,18 @@
+#pragma once
+#include"data_structure.h"
 
+void getURL(char* rev, char* url)
+{
+	int i = 0, j = 0, k = 0,length=strlen(rev);
+	while (i<length)
+	{
+		if (rev[i] > 0 && rev[i] <= 63)
+		{
+			for (j = rev[i], i++; j > 0; i++, k++, j--)
+				url[k] = rev[i];
+			if (rev[i] != 0)
+				url[k++] = '.';
+		}
+	}
+	url[k] = 0;
+}
