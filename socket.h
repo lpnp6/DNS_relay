34bfb74e7e47ab0d349a886ffb96e106feff2de4
lpnp6 <117889360+lpnp6@users.noreply.github.com>
2023-06-24 00:24:34 +0800

    Update socket.h

--- socket.h
+++ socket.h
@@ -1,11 +1,14 @@
 #pragma once
 #pragma comment(lib,"wsock32.lib")
 #include<winsock2.h>
+#include<stdio.h>
+#define IP_LENGTH 16
 
-#define Local_DNS_Address "127.0.0.1"
-#define Server_DNS_Address "10.3.9.45"
+WSAData Wsadata;
+char serverIPAddress[IP_LENGTH] = "10.3.9.45";
+char clientIPAddress[IP_LENGTH] = "127.0.0.1";
 
-SOCKET CreateSocket(short sin_family, u_short sin_port,const char* sin_addr)
+SOCKET CreateSocket(short sin_family, u_short sin_port,char* sin_addr)
 {
 	SOCKET Socket = socket(AF_INET, SOCK_DGRAM, 0);
 	u_long FAR arg = 1;
@@ -14,7 +17,13 @@ SOCKET CreateSocket(short sin_family, u_short sin_port,const char* sin_addr)
 	addr.sin_family = sin_family;
 	addr.sin_port = htons(sin_port);
 	addr.sin_addr.S_un.S_addr = inet_addr(sin_addr);
-	bind(Socket, (sockaddr*)&addr, sizeof(addr));
+	if (bind(Socket, (sockaddr*)&addr, sizeof(addr)) < 0)
+	{
+		printf("Fail to bind UDP port\n");
+		exit(0);
+	}
+	else
+		printf("Bind UDP port 53 ...OK!\n");
 	return Socket;
 }
 
