d878557b34a48a111938773cf4847728b2492e57
lpnp6 <117889360+lpnp6@users.noreply.github.com>
2023-06-24 13:42:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,5 @@ int main(int argc,char* argv[])
 {
 	WSAStartup(MAKEWORD(2, 2), &Wsadata);
 	setParameter(argc, argv);
-	SOCKET serverSocket = CreateSocket(AF_INET, 53, clientIPAddress);
-	SOCKET clientSocket = CreateSocket(AF_INET, 53, serverIPAddress);
+	SOCKET serverSocket = CreateSocket();
 }
