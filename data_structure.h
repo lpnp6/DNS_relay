90bb0bfb6a9f2c3ee22b2cc5ae56a988b1ff77b9
蒋礼特 <102312108+GiteJianl@users.noreply.github.com>
2023-06-24 12:06:50 +0800

    Update data_structure.h

--- data_structure.h
+++ data_structure.h
@@ -68,5 +68,6 @@ typedef struct ID_change
 
 extern ip_domain_trans DNS_domain_Table[AMOUNT];		//DNS域名解析表
 extern ID_trans IDTransTable[AMOUNT];	//ID转换表
+int IDcount=0;
 int Day, Hour, Minute, Second, Milliseconds;//保存系统时间的变量
 
