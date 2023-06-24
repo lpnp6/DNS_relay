4623cc37c3645592f87e09498e7d48291e5624e3
2021211785 <105899128+2021211785@users.noreply.github.com>
2023-06-24 13:29:34 +0800

    Update Header.h

--- Header.h
+++ Header.h
@@ -7,7 +7,7 @@
 #include <windows.h>
 #include <time.h>
 #define AMOUNT 1000
-
+#define lengthURL 64 //最大域名长
 typedef struct {
     uint16_t ID; // Identification
     uint16_t id;
@@ -79,4 +79,4 @@ extern ip_domain_trans DNS_domain_Table[AMOUNT];		//DNS域名解析表
 extern ID_trans IDTransTable[AMOUNT];	//ID转换表
 int IDcount=0;
 int Day, Hour, Minute, Second, Milliseconds;//保存系统时间的变量
-
+char Url[lengthURL];
