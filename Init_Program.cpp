ae4830577c2fae41c5ea9d8186fb28798557dd3a
lpnp6 <117889360+lpnp6@users.noreply.github.com>
2023-06-24 20:12:14 +0800

    Update Init_Program.cpp

--- Init_Program.cpp
+++ Init_Program.cpp
@@ -1,15 +1,15 @@
 #pragma warning (disable:4996)
 #include"Header.h"
 
-char serverIPAddress[IP_LENGTH];
-char clientIPAddress[IP_LENGTH];
+char LocalDNSAddress[IPLength];
+char DefDNSAddress[IPLength];
 char filePath[MAX_FILE_LENGTH];
 int DebugLevel;
 
 void Init()
 {
-	strcpy(serverIPAddress, "10.3.9.45");
-	strcpy(clientIPAddress, "127.0.0.1");
+	strcpy(DefDNSAddress, "10.3.9.44");
+	strcpy(LocalDNSAddress, "127.0.0.1");
 	strcpy(filePath, "dnsrelay.txt");
 	DebugLevel = 0;
 }
@@ -28,7 +28,7 @@ void setParameter(int argc, char* argv[])
 		}
 		else
 		{
-			strcpy(serverIPAddress, argv[1]);
+			strcpy(DefDNSAddress, argv[1]);
 		}
 	}
 	else if (argc == 3)
@@ -39,23 +39,23 @@ void setParameter(int argc, char* argv[])
 				DebugLevel = 1;
 			if (argv[1][2] == 'd')
 				DebugLevel = 2;
-			strcpy(serverIPAddress, argv[2]);
+			strcpy(DefDNSAddress, argv[2]);
 		}
 		else
 		{
-			strcpy(serverIPAddress, argv[1]);
+			strcpy(DefDNSAddress, argv[1]);
 			strcpy(filePath, argv[2]);
 		}
 	}
-	else if(argc==4)
+	else if (argc == 4)
 	{
 		if (argv[1][1] == 'd')
 			DebugLevel = 1;
 		if (argv[1][2] == 'd')
 			DebugLevel = 2;
-		strcpy(serverIPAddress, argv[2]);
+		strcpy(DefDNSAddress, argv[2]);
 		strcpy(filePath, argv[3]);
 	}
-	printf("Name server %s:53\n", serverIPAddress);
+	printf("Name server %s:53\n", DefDNSAddress);
 	printf("Debug level %d\n", DebugLevel);
 }
